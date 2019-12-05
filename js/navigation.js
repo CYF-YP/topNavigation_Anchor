64169acea41d64e3ff36246bfe51cc4f75599e17
CYF <914621447@qq.com>
2019-12-05 11:08:33 +0800

    代码优化

--- js/navigation.js
+++ js/navigation.js
@@ -14,14 +14,15 @@
         self.ele_width = self.ele_width;
 
         self.setAnchorPoint(self.nav_bar, self.container);
-        self.onScrollLevel(self.nav_bar, self.ele_width, window);
-        self.onScrollVertical(self.nav_bar, self.container, window);
+        self.onScrollVertical(self.nav_bar, self.container, self.ele_width, window);
+        self.navClick(self.nav_bar, self.ele_width, window);
     }
 
     // 原型链上提供的方法
     navigation.prototype = {
         // 设置锚点
         setAnchorPoint: function (ele_nav, ele_container) {
+            // 导航栏初始设置
             $(ele_container).find('div[data-anchor="true"]').each(function (index, element) {
                 $(element).attr('id', 'nav_link_' + index);
                 $(element).addClass('hidden_nav');
@@ -29,70 +30,48 @@
             // 锚点对上偏移导航高度
             var nav_height = $(ele_nav).outerHeight();
             $('.hidden_nav').each(function () {
-                $(this).css({ 'top': -(nav_height * 2 - 5) });
+                $(this).css({ 'top': -nav_height });
             });
-            
+            // 遍历导航栏按钮,并添加锚链接,index
             $(ele_nav).children().eq(0).find('.nav_bar_item').each(function (index, element) {
                 $(element).attr('href', '#nav_link_' + index);
-            });
-
-            $(ele_nav).children().eq(0).find('.nav_bar_item').each(function (index, element) {
-                $(this).on('click', function () {
-                    var obj = $(this);
-                    obj.addClass('linkActive').siblings().removeClass('linkActive');
-                    if (index == 0) {
-                        $(ele_nav).css({ 'position': 'relative', 'top': 'auto', 'z-index': 9 });
-                    } else {
-                        $(ele_nav).css({ 'position': 'fixed', 'top': '0', 'z-index': 99 });
-                    }
-                });
+                $(element).attr('data-navIndex', index);
             });
         },
         // 导航栏水平滑动
-        onScrollLevel: function (ele_nav, ele_width, window) {
-            $(window).scroll(function () {
-                var temNavList = [];
-                var activityIndex = null;
-                $(ele_nav).children().eq(0).find('.nav_bar_item').each(function (index, element) {
-                    temNavList.push($(element).width());
-                    if ($(this).hasClass('linkActive')) {
-                        activityIndex = index + 1;
-                    }
-                });
-
-                // ele_width是每个元素宽度
-                if (activityIndex) {
-                    // $(ele_nav).children().eq(0).scrollLeft((activityIndex - 1) * ele_width);
-                    var timer = setTimeout(function () {
-                        $(ele_nav).children().eq(0).stop().animate({
-                            scrollLeft: (activityIndex - 1) * ele_width + 'px'
-                        }, 'fast', 'linear');
-                        clearTimeout(timer);
-                    }, 100);
-                }
-            });
+        onScrollLevel: function (ele_nav, ele_width) {
+            self.activityIndex = null;
+            self.activityIndex = Number($('.linkActive').eq(0).attr('data-navIndex')) + 1;
+            if (activityIndex != null) {
+                // $(ele_nav).children().eq(0).scrollLeft((self.activityIndex - 1) * ele_width);
+                var timer = setTimeout(function () {
+                    $(ele_nav).children().eq(0).stop().animate({
+                        scrollLeft: (self.activityIndex - 1) * ele_width + 'px'
+                    }, 'fast', 'linear');
+                    clearTimeout(timer);
+                }, 100);
+            }
         },
         // 内容竖直方向滑动
-        onScrollVertical: function (ele_nav, ele_container, window) {
+        onScrollVertical: function (ele_nav, ele_container, ele_width, window) {
+            var _this = this;
             $(window).scroll(function () {
+                _this.onScrollLevel(ele_nav, ele_width);
                 var top = $(window).scrollTop();
-                var temlist = [];
+                self.temlist = [];
                 if (top >= ($(ele_nav).prev().outerHeight() + $(ele_nav).prev().offset().top)) {
                     $(ele_nav).css({ 'position': 'fixed', 'top': '0', 'z-index': 99 });
                     $(ele_container).find('div[data-anchor="true"]').each(function (index, element) {
-                        temlist.push($(element).offset().top);
+                        self.temlist.push($(element).offset().top);
                     });
-                    temlist.map(function (value, index, arr) {
-                        // 10为设置的多的偏移量,防止刚刚好滑到这个地方时出错
-                        if (value <= top + 10) {
+                    self.temlist.map(function (value, index, arr) {
+                        if (value <= top + $(ele_nav).outerHeight()) {
                             if (index != arr.length - 1) {
-                                if (temlist[index + 1] > top + 10) {
+                                if (self.temlist[index + 1] > top + $(ele_nav).outerHeight()) {
                                     $($(ele_nav).children().eq(0).find('.nav_bar_item')[index]).addClass('linkActive').siblings().removeClass('linkActive');
                                 }
                             } else {
-                                if (temlist[index] <= top + 10) {
-                                    $($(ele_nav).children().eq(0).find('.nav_bar_item')[index]).addClass('linkActive').siblings().removeClass('linkActive');
-                                }
+                                $($(ele_nav).children().eq(0).find('.nav_bar_item')[index]).addClass('linkActive').siblings().removeClass('linkActive');
                             }
                         }
                     });
@@ -100,6 +79,24 @@
                     $(ele_nav).css({ 'position': 'relative', 'top': 'auto', 'z-index': 9 });
                     $($(ele_nav).children().eq(0).find('.nav_bar_item')[0]).addClass('linkActive').siblings().removeClass('linkActive');
                 }
+            });
+        },
+        // 导航栏按钮点击事件
+        navClick: function (ele_nav, ele_width, window) {
+            var _this = this;
+            $(ele_nav).children().eq(0).find('.nav_bar_item').on('click', function () {
+                var obj = $(this);
+                obj.addClass('linkActive').siblings().removeClass('linkActive');
+                if (obj.attr('data-navIndex') == 0) {
+                    $(ele_nav).css({ 'position': 'relative', 'top': 'auto', 'z-index': 9 });
+                    var timer = setTimeout(function () {
+                        $(window).scrollTop(0);
+                        clearTimeout(timer);
+                    }, 100);
+                } else {
+                    $(ele_nav).css({ 'position': 'fixed', 'top': '0', 'z-index': 99 });
+                }
+                _this.onScrollLevel(ele_nav, ele_width);
             });
         }
     }
